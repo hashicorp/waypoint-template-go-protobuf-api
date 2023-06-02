3267cb8d581004f55f39e5d10003e5ce9b86218f
Joseph Rajewski <83741749+paladin-devops@users.noreply.github.com>
2023-06-02 13:48:23 -0400

    init: Go protobuf API template app.

---
+++ cmd/__wp_project__-api/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/%%gh_org%%/%%wp_project%%/config"
+	%%wp_project%%v1 "github.com/%%gh_org%%/%%wp_project%%/gen/proto/go/%%wp_project%%/v1"
+	"github.com/%%gh_org%%/%%wp_project%%/server"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+)
+
+func main() {
+	fmt.Println("starting %%wp_project%%.......")
+	defer fmt.Println("%%wp_project%% exiting!")
+
+	err := serve()
+	if err != nil {
+		log.Fatalf("failed serving\n%s", err)
+	}
+}
+
+func serve() error {
+	var c config.Config
+
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath != "" {
+		var err error
+		c, err = config.GetConfig(configPath)
+		if err != nil {
+			return errors.Wrapf(err, "failed to load config from %q", configPath)
+		}
+	} else {
+		// Erroring on no config would be totally valid
+		// return errors.New("Environment variable CONFIG_PATH is unset")
+
+		c = config.DefaultConfig()
+	}
+
+	// Start the service
+	%%wp_project%%Server, err := server.New%%Wp_project%%Server(c.%%Wp_project%%)
+	if err != nil {
+		return errors.Wrapf(err, "failed to start %%wp_project%% server")
+	}
+
+	listener, err := net.Listen("tcp", c.Server.BindAddr)
+	if err != nil {
+		return errors.Wrapf(err, "failed to listen on %s", c.Server.BindAddr)
+	}
+	grpcServer := grpc.NewServer()
+	grpc_health_v1.RegisterHealthServer(grpcServer, health.NewServer())
+
+	%%wp_project%%v1.Register%%Wp_project%%ServiceServer(grpcServer, %%wp_project%%Server)
+	reflection.Register(grpcServer)
+
+	log.Printf("Connecting to Postgres")
+	type conn struct {
+		user     string
+		password string
+		port     int
+		host     string
+		dbname   string
+	}
+
+	port, err := strconv.Atoi(os.Getenv("DATABASE_PORT"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	connection := conn{
+		user:     os.Getenv("DATABASE_USERNAME"),
+		password: os.Getenv("DATABASE_PASSWORD"),
+		port:     port,
+		host:     os.Getenv("DATABASE_HOSTNAME"),
+		dbname:   os.Getenv("DATABASE_NAME"),
+	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		connection.host, connection.port, connection.user, connection.password, connection.dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Serving on %q", c.Server.BindAddr)
+	if err := grpcServer.Serve(listener); err != nil {
+		return errors.Wrapf(err, "gRPC server exited")
+	}
+
+
+
+	return nil
+}
